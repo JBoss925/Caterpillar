b36ce99558eba86d7135391f5d96cb6519d86bbf
Jagger <jaggerbrulato@gmail.com>
2016-06-24 22:52:36 -0400

    Working on a small bit of code.

--- src/me/JBoss925/caterpillar/fragments/RelativeCoordinates.java
+++ src/me/JBoss925/caterpillar/fragments/RelativeCoordinates.java
@@ -21,7 +21,7 @@ public class RelativeCoordinates {
         this.z = z;
     }
 
-    public static List<Location> getAbsoluteLocations(List<RelativeCoordinates> list, Location origin){
+    public static Iterable<Location> getAbsoluteLocations(Iterable<RelativeCoordinates> list, Location origin){
         List<Location> returnList = new ArrayList<Location>();
         for(RelativeCoordinates r : list){
             double tempBlockX = (double) origin.getBlockX() + r.getX();
@@ -33,7 +33,7 @@ public class RelativeCoordinates {
         return returnList;
     }
 
-    public static List<RelativeCoordinates> getRelativeCoordinates(List<Location> locs, Location origin){
+    public static Iterable<RelativeCoordinates> getRelativeCoordinates(Iterable<Location> locs, Location origin){
         List<RelativeCoordinates> relativeCoords = new ArrayList<RelativeCoordinates>();
         for(Location loc : locs){
             int tempRelX = loc.getBlockX() - origin.getBlockX();
@@ -45,7 +45,7 @@ public class RelativeCoordinates {
         return relativeCoords;
     }
 
-    public static List<Location> getAbsoluteLocationsWithinTwoLocations(Location loc1, Location loc2){
+    public static Iterable<Location> getAbsoluteLocationsWithinTwoLocations(Location loc1, Location loc2){
         double startx = (double) Math.min(loc1.getBlockX(), loc2.getBlockX());
         double endx = (double) Math.max(loc1.getBlockX(), loc2.getBlockX());
         double starty = (double) Math.min(loc1.getBlockY(), loc2.getBlockY());
@@ -69,6 +69,26 @@ public class RelativeCoordinates {
         double middlez = (double) Math.min(loc1.getBlockZ(), loc2.getBlockZ()) + (Math.max(loc1.getBlockZ(), loc2.getBlockZ()) - Math.min(loc1.getBlockZ(), loc2.getBlockZ()));
         return new Location(loc1.getWorld(), middlex, middley, middlez);
     }
+//
+//    public static Iterable<Location> rotateLocationsCounterClockwiseXAboutOrigin(Double degrees, Iterable<Location> locs, Location origin){
+//        Iterable<RelativeCoordinates> newLocs = getRelativeCoordinates(locs, origin);
+//        for (RelativeCoordinates r : newLocs){
+//            Double preRadians = Math.acos(r.getX());
+//            Double radians = preRadians + Math.toRadians(degrees);
+//            double newX = Math.cos(radians);
+//            double newY = Math.sin(radians);
+//            double newZ =
+//        }
+//    }
+//
+//    public static Iterable<Location> rotateLocationsCounterClockwiseX(Iterable<Location> locs, Double degrees){
+//        double smallX;
+//        double bigX;
+//        for(Location loc: locs){
+//            if()
+//        }
+//        return;
+//    }
 
 
     public int getX() {
